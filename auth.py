60d4e3ef7e24139eba0ee993fc32df5b1bfd4145
techvyana20-oss <techvyana2.0@gmail.com>
2026-02-22 10:52:38 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,20 @@
+import hashlib
+import sqlite3
+from config import DB_NAME
 
+def create_user(username, password, role):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+    c.execute("INSERT INTO users VALUES (?, ?, ?)", (username, hashed, role))
+    conn.commit()
+    conn.close()
+
+def login(username, password):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+    c.execute("SELECT role FROM users WHERE username=? AND password=?", (username, hashed))
+    result = c.fetchone()
+    conn.close()
+    return result[0] if result else None
