5ee7aced12ccb519a3acc6f39aa481b4d7fdc752
techvyana20-oss <techvyana2.0@gmail.com>
2026-02-22 10:52:23 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,33 @@
+import sqlite3
+from config import DB_NAME
 
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT PRIMARY KEY,
+        password TEXT,
+        role TEXT
+    )
+    """)
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS activity (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        tool TEXT,
+        user TEXT,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+def log_activity(tool, user):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    c.execute("INSERT INTO activity (tool, user) VALUES (?, ?)", (tool, user))
+    conn.commit()
+    conn.close()
