47b683c3096ede089311d622d71ecd6e03f6a312
techvyana20-oss <techvyana2.0@gmail.com>
2026-02-22 10:55:58 +0530

    Update crypto.py

--- core/crypto.py
+++ core/crypto.py
@@ -1,1 +1,20 @@
+from cryptography.fernet import Fernet
 
+def generate_key():
+    return Fernet.generate_key()
+
+def encrypt_file(filepath, key):
+    fernet = Fernet(key)
+    with open(filepath, "rb") as f:
+        data = f.read()
+    encrypted = fernet.encrypt(data)
+    with open(filepath, "wb") as f:
+        f.write(encrypted)
+
+def decrypt_file(filepath, key):
+    fernet = Fernet(key)
+    with open(filepath, "rb") as f:
+        data = f.read()
+    decrypted = fernet.decrypt(data)
+    with open(filepath, "wb") as f:
+        f.write(decrypted)
