b9b3deb00a7f7c1e3367ce82f98b07a5aee1b184
techvyana20-oss <techvyana2.0@gmail.com>
2026-02-22 10:58:01 +0530

    Update hash_plugin.py

--- plugins/hash_plugin.py
+++ plugins/hash_plugin.py
@@ -1,1 +1,6 @@
+import hashlib
 
+def run(user):
+    text = input("Enter text: ")
+    print("SHA256:", hashlib.sha256(text.encode()).hexdigest())
+    print("MD5:", hashlib.md5(text.encode()).hexdigest())
