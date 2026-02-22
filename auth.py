8831d399aae5594d4ab5bb3f1a6420d0e4581e6a
techvyana20-oss <techvyana2.0@gmail.com>
2026-02-22 11:16:22 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,5 +1,8 @@
+import hashlib
+import sqlite3
+from config import DB_NAME
+
 def create_user(username, password, role):
-    import sqlite3
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
     hashed = hashlib.sha256(password.encode()).hexdigest()
@@ -12,3 +15,17 @@ def create_user(username, password, role):
         print("Username already exists. Please choose another.")
     finally:
         conn.close()
+
+def login(username, password):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+
+    c.execute("SELECT role FROM users WHERE username=? AND password=?", (username, hashed))
+    result = c.fetchone()
+
+    conn.close()
+
+    if result:
+        return result[0]
+    return None
