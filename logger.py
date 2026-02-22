52751a1e9f37af6ba64e4eaef6ec42bc0ed49155
techvyana20-oss <techvyana2.0@gmail.com>
2026-02-22 10:52:50 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,1 +1,10 @@
+import logging
 
+logging.basicConfig(
+    filename="tv_lab.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def log_info(message):
+    logging.info(message)
