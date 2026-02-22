6cdb9cd4a41370809c50f7f84dfe54f60ba43a61
techvyana20-oss <techvyana2.0@gmail.com>
2026-02-22 10:57:48 +0530

    Update password_plugin.py

--- plugins/password_plugin.py
+++ plugins/password_plugin.py
@@ -1,1 +1,12 @@
+import secrets
+import string
+from core.entropy import calculate_entropy
 
+def run(user):
+    length = int(input("Password length: "))
+    charset = string.ascii_letters + string.digits + "!@#$%^&*"
+    pwd = ''.join(secrets.choice(charset) for _ in range(length))
+    entropy = calculate_entropy(len(charset), length)
+
+    print("Generated:", pwd)
+    print("Entropy:", round(entropy,2), "bits")
